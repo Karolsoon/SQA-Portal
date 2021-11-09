2d66bc7b74fb603cc112c56a6947caa7f581d0ab
Karolsoon <90521206+Karolsoon@users.noreply.github.com>
2021-11-09 23:34:07 +0100

    Changed settings to suit PostgreSQL, initially defined Models

--- SupplierQuality/SQA/models.py
+++ SupplierQuality/SQA/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.utils import timezone
 
-# Create your models here.
+class Supplier_T1(models.Model):
+    name = models.CharField(max_length=100)
+    valid_from = models.DateField()
+    is_9001 = models.BooleanField(default=False)
+    is_17494 = models.BooleanField(default=False)
+
+class Supplier_T2(models.Model):
+    supplier_t1 = models.ForeignKey(Supplier_T1)
+
+class Supplier_T3(models.Model):
+    supplier_t1 = models.ForeignKey(Supplier_T1)
+    supplier_t2 = models.ForeignKey(Supplier_T2)
+
+class Subassembly(models.Model):
+    part_number = models.CharField(max_length=50)
+    part_name = models.CharField(max_length=50)
+
+class Part(models.Model):
+    part_name = models.CharField(max_length=50)
+    part_number = models.CharField(max_length=30)
+    subassy = models.ForeignKey(Subassembly)
+    supplier_t1 = models.ForeignKey(Supplier_T1, on_delete=models.CASCADE)
+    supplier_t2 = models.ForeignKey(Supplier_T1, on_delete=models.CASCADE)
+    supplier_t3 = models.ForeignKey(Supplier_T1, on_delete=models.CASCADE)
+    valid_from = models.DateField('validation date')
+    is_valid = models.BooleanField(default=False)
+    requalification = models.DateField(valid_from)
+
+class PPAP(models.Model):
+    part = models.ForeignKey(Part.part_number, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=5)
--- SupplierQuality/SupplierQuality/settings.py
+++ SupplierQuality/SupplierQuality/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'SupplierQuality.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'supplierDB',
+        'USER': 'postgres',
+        'PASSWORD': 'kardol09',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USE_TZ': 'True',
+        'client_encoding': 'UTF8',
+        'default_transaction_isolation': 'read committed',
     }
 }
 
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
